757a833a00959861b723e186b4140d5cf667f113
Refri Rifwan <refririfwan@inovasi360.com>
2022-01-20 14:34:09 +0700

    Create Structure Project

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+import javax.print.PrintService;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+  public static String[] model = new String[10];
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+  }
+
+  // Show Todo List
+  public static void showTodoList() {
+
+  }
+
+  // Add new Todo List
+  public static void addTodoList() {
+
+  }
+
+  // Delete a Todo List
+  public static void removeTodoList() {
+
+  }
+
+  // Showing Todo List Menu
+  public static void viewShowTodoList() {
+
+  }
+
+  // Showing Add Todo List
+  public static void viewAddTodoList() {
+
+  }
+
+  // Showing Delete Todo List
+  public static void viewRemoveTodoList() {
+
+  }
 }
