a93ed2639e654ce37ad3e848670b2afbd893be62
Refri Rifwan <refririfwan@inovasi360.com>
2022-01-25 11:02:38 +0700

    Test Repository & Service : Menambah Todolist

--- src/Test/service/TodoListServiceTest.java
+++ src/Test/service/TodoListServiceTest.java
@@ -1,13 +1,15 @@
 package Test.service;
 
 import entity.TodoList;
+import repository.TodoListRepository;
 import repository.TodoListRepositoryImpl;
 import service.TodoListService;
 import service.TodoListServiceImpl;
 
 public class TodoListServiceTest {
   public static void main(String[] args) {
-    testShowTodoList();
+    // testShowTodoList();
+    testAddTodoList();
   }
 
   public static void testShowTodoList() {
@@ -17,6 +19,17 @@ public class TodoListServiceTest {
     todoListRepository.data[2] = new TodoList("Learn Jakarta");
 
     TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+    todoListService.showTodoList();
+  }
+
+  public static void testAddTodoList() {
+    TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+    TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+    todoListService.addTodoList("Learn Java");
+    todoListService.addTodoList("Learn Lombok");
+    todoListService.addTodoList("Learn Jakarta");
 
     todoListService.showTodoList();
   }
--- src/repository/TodoListRepositoryImpl.java
+++ src/repository/TodoListRepositoryImpl.java
@@ -65,5 +65,4 @@ public class TodoListRepositoryImpl implements TodoListRepository {
     }
 
   }
-
 }
